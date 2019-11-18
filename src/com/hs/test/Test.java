5d85f15c87fab9e1b4f26871e3998093bbbecff4
chenshuai <1448509198@qq.com>
2019-11-18 15:38:18 +0800

    修改内容

--- src/com/hs/test/Test.java
+++ src/com/hs/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         String hsbzls="040200F2004TCE";
         System.out.println(hsbzls.substr(0,2);
+		System.out.println("Hello World");
     }
 }
