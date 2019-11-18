2585539e7436e1ecc616567eb79cc9ac785b59c3
chenshuai <1448509198@qq.com>
2019-11-18 15:36:39 +0800

    第一次提交

---
+++ src/com/hs/test/Test.java
@@ -0,0 +1,8 @@
+package com.hs.test;
+
+public class Test {
+    public static void main(String[] args) {
+        String hsbzls="040200F2004TCE";
+        System.out.println(hsbzls.substr(0,2);
+    }
+}
